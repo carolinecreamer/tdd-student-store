6cbf7c66ee3822121734c8f3bb3f6d770d2891b2
Caroline Creamer <ccreamer@fb.com>
2022-06-22 16:35:16 -0700

    ProductView component

--- student-store-ui/src/components/ProductDetail/ProductView.jsx
+++ student-store-ui/src/components/ProductDetail/ProductView.jsx
@@ -1,10 +1,13 @@
 import * as React from "react"
 import "./ProductView.css"
+import ProductCard from "./ProductCard"
 
-export default function ProductView() {
+export default function ProductView({product, productId, quantity, handleAddItemToCart, handleRemoveItemToCart}) {
   return (
-    <div className="home">
-      <p>ProductView</p>
+    <div className="product-view">
+      <h1 className="product-id">Product #{productId}</h1>
+      <ProductCard product={product} productId={productId} quantity={0} handleAddItemToCart={handleAddItemToCart} handleRemoveItemToCart={handleRemoveItemToCart} showDescription={true}/>
+    
     </div>
   )
 }
