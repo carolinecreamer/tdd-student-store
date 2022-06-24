ac520788c275a250c25f3c4ea179cb3caae42d8b
Caroline Creamer <ccreamer@fb.com>
2022-06-23 18:08:49 -0700

    calculates total

--- student-store-ui/src/components/Sidebar/ShoppingCart.jsx
+++ student-store-ui/src/components/Sidebar/ShoppingCart.jsx
@@ -2,6 +2,11 @@ import * as React from "react"
 import "./ShoppingCart.css"
 
 export default function Sidebar({isOpen, products, shoppingCart}) {
+    let cost = 0;
+    shoppingCart.map((item, idx) => 
+        cost += (products.find((i)=>i.id === item.itemId)).price * item.quantity
+    )
+
     if (shoppingCart.length == 0) {
         return (
             <p className="notification">No items added to cart yet. Start shopping now!</p>
@@ -18,6 +23,19 @@ export default function Sidebar({isOpen, products, shoppingCart}) {
                     <span className="cart-product-unit-price">${(((products.find((i)=>i.id === item.itemId)).price)*item.quantity).toFixed(2)}</span>
                 </div>
             )}
+
+            <div className="subtotal">
+                <span>Subtotal</span>
+                <span>${cost.toFixed(2)}</span>
+            </div>
+            <div>
+                <span>Taxes and Fees</span>
+                <span>${(cost*0.0875).toFixed(2)}</span>
+            </div>
+            <div>
+                <span>Total</span>
+                <span>${(cost + (cost * 0.0875)).toFixed(2)}</span>
+            </div>
         </div>
         )
     }
